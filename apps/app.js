12b993ac178a1f4212c2590c2afa47288c4c9a17
leoreq <leoreq@gmail.com>
2015-05-09 16:33:02 -0400

    first commit

---
+++ apps/app.js
@@ -0,0 +1,61 @@
+$(function(){
+
+
+    $('#search-term').submit(function(event){
+        event.preventDefault();
+        var searchTerm=$('#query').val();
+        getRequest(searchTerm);
+    })
+        /* 
+        //GET METHOD 2
+        $.get('http://omdbapi.com/?s=sex&r=json',function(suData){
+            sData=suData.Search;
+            console.log(sData);
+        },'json')
+        */
+});
+
+
+
+//PRINT ALL ELEMENTS METHOD 2
+function showresults (sData) {
+    /*
+    //PRINT ALL ELEMENTS METHOD 1
+        for (var i=0;i<sData.length;i++)
+        {
+            console.log("DEBUG:"+sData[i].Title);
+        };
+    */
+    $.each(sData,function(index,line)
+            {
+                
+                //console.log("1st"+line);
+                console.log("DB2:"+line.Title);
+                $("#search-results").append("<p>"+line.Title+"</p>");
+            });
+};
+
+function showresults2 (sData) {
+    var items="";
+    $.each(sData,function(index,line)
+            {
+                items+="<p>"+line.Title+"</p>"
+                            
+                //console.log("1st"+line);
+                console.log("DB2:"+line.Title);
+
+            });
+    $("#search-results").html(items);   
+};
+
+function getRequest (searchTerm){
+    var params = {
+        s:searchTerm,
+        r:'json'
+    }
+    console.log(params);
+    url='www.omdbapi.com/';
+    $.getJSON(url,params,function(sData){
+        showresults2(sData.Search);
+    });
+};
